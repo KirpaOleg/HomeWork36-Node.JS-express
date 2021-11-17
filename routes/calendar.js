e3f7ba6837a66c3b59067f2701928b44fbc9d976
KirpaOleg <profix.ok@gmail.com>
2021-11-17 21:01:13 +0200

    all3

--- routes/calendar.js
+++ routes/calendar.js
@@ -27,11 +27,44 @@ router.get('/:year/:month/', (req, res) => {
     'Воскресенье',
   ];
 
+  console.log(weekArr[0]);
+
   String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
   }
 
   let monthStr = moment().month(profileObj.month).format("MMMM").capitalize();
+
+  const getTotalCellCount = (tempObj) => {
+    const sumStart = tempObj.daysOfMonth + tempObj.firstDay;
+    return (7 - (sumStart % 7)) + sumStart;
+  };
+  
+  const generateTable = (tempObj, count) => {
+    let strMain = '';
+    let strTr = '';
+    let dayNumber = 0;
+    let weekend = (7 - ((tempObj.daysOfMonth + tempObj.firstDay) % 7)) + (tempObj.daysOfMonth + tempObj.firstDay)
+    for (let i = 0; i <= count; i++) {
+      if ((i % 7) === 0 && i !== 0) {
+        strMain += `<tr>${strTr}</tr>\n`;
+        dayNumber++
+        strTr = `<td>${dayNumber}</td>`;
+        console.log(weekend);
+      } else {
+        if (i >= tempObj.firstDay && i < (tempObj.daysOfMonth + tempObj.firstDay)) {
+          dayNumber++
+          strTr += `<td>${dayNumber}</td>`;
+        } else {
+          strTr += `<td class="no-data"></td>`;
+        } 
+      }
+    }
+    strMain = `<table>
+    <tr class="header"><td>${weekArr[0]}</td><td>${weekArr[1]}</td><td>${weekArr[2]}</td><td>${weekArr[3]}</td><td>${weekArr[4]}</td><td>${weekArr[5]}</td><td>${weekArr[6]}</td></tr>
+    ${strMain}</table>`;
+    return strMain;
+  }
 
   const cellCount = getTotalCellCount(profileObj);
   const table = generateTable(profileObj, cellCount)
@@ -39,35 +72,6 @@ router.get('/:year/:month/', (req, res) => {
   res.render('calendar', {...profileObj, weekArr, monthStr, table})
 });
 
-const getTotalCellCount = (tempObj) => {
-  const sumStart = tempObj.daysOfMonth + tempObj.firstDay;
-  return (7 - (sumStart % 7)) + sumStart;
-};
 
-const generateTable = (tempObj, count) => {
-  let strMain = '';
-  let strTr = '';
-  let dayNumber = 0;
-  let weekend = (7 - ((tempObj.daysOfMonth + tempObj.firstDay) % 7)) + (tempObj.daysOfMonth + tempObj.firstDay)
-  for (let i = 0; i <= count; i++) {
-    if ((i % 7) === 0 && i !== 0) {
-      strMain += `<tr>${strTr}</tr>\n`;
-      dayNumber++
-      strTr = `<td>${dayNumber}</td>`;
-      console.log(weekend);
-    } else {
-      if (i >= tempObj.firstDay && i < (tempObj.daysOfMonth + tempObj.firstDay)) {
-        dayNumber++
-        strTr += `<td>${dayNumber}</td>`;
-      } else {
-        strTr += `<td class="red"></td>`;
-      } 
-    }
-  }
-  strMain = `<table>
-  
-  ${strMain}</table>`;
-  return strMain;
-}
 
 module.exports = router;  
